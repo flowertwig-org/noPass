5c5a3dfe50ef76a8a70110df690e1ee09b5e427c
Mattias Flowertwig <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2015-02-02 22:31:59 +0100

    Updated github.com logic

--- sites/github.com-contentscript.js
+++ sites/github.com-contentscript.js
@@ -1,49 +1,106 @@
 // The background page is asking us to find an address on the page.
 if (window == top) {
-    chrome.extension.onMessage.addListener(function (options, sender, sendResponse) {
-        switch (options.action) {
-            case 'profile':
-                var profile = options.profile;
-                switch (profile.hostname) {
-                    case "github.com":
-                        var address = '' + document.location;
-                        // Fill out remind of password form.
-                        var isReminder = options.profileType.remindUrl == document.location;
-                        if (isReminder) {
-                            var userIdElement = $('#email_field');
-                            if (userIdElement.length) {
-                                userIdElement.val(profile.userId);
-                                var form = userIdElement.parents('form');
-                                var btn = form.find('[type="submit"]');
+
+    $(document).ready(function () {
+        chrome.runtime.sendMessage({
+            'action': 'matched',
+            'hostname': document.location.hostname
+        }, function (options) {
+            var profile = options.profile;
+            var progress = options.progress;
+
+            if (!progress) {
+                return;
+            }
+
+            // If tab id doesn't match we are not interested (This is to make sure we are not changing any of the tabs our user is using
+            if (progress.currentTab != options.tabId) {
+                return;
+            }
+            //console.log('same tab');
+
+            switch (progress.status) {
+                case 'remindPass':
+                    var address = '' + document.location;
+                    var isReminder = profile.remindUrl == address;
+                    if (isReminder) {
+                        var userIdElement = $('#email_field');
+                        if (userIdElement.length) {
+                            userIdElement.val(profile.userId);
+                            var form = userIdElement.parents('form');
+                            var btn = form.find('[type="submit"]');
+
+                            chrome.runtime.sendMessage({
+                                'action': 'updateStatus',
+                                'status': 'changeDefaultPass'
+                            }, function () {
                                 btn.click();
-                            }
-                            sendResponse(true);
-                        } else if (address.indexOf("/password_reset/") >= 0) {
-                            var newPass = "N0tSoStrongPassw0rd";
+                                setTimeout(function () {
+                                    chrome.runtime.sendMessage({
+                                        'action': 'closeTab',
+                                        'tabId': progress.currentTab
+                                    });
+                                }, 1000);
+                            });
+                        }
+                    }
+                    break;
+                case 'changeDefaultPass':
+                    var address = '' + document.location;
+                    if (address.indexOf('/password_reset/') >= 0) {
+                        chrome.runtime.sendMessage({
+                            'action': 'genPass'
+                        }, function (pass) {
+
+                            var userPassElements = $('#password,#password_confirmation');
+                            userPassElements.val(pass);
+
+                            chrome.runtime.sendMessage({
+                                'action': 'updateData',
+                                'data': pass
+                            }, function () {
+                                chrome.runtime.sendMessage({
+                                    'action': 'updateStatus',
+                                    'status': 'login'
+                                }, function () {
+                                    var form = userPassElements.parents('form');
+                                    var btn = form.find('[type="submit"]');
+                                    btn.click();
+                                    setTimeout(function () {
+                                        chrome.runtime.sendMessage({
+                                            'action': 'closeTab',
+                                            'tabId': progress.currentTab
+                                        });
+                                    }, 1000);
+                                });
+                            });
+                        });
+                    }
+                    break;
+                case 'login':
+                    var address = '' + document.location;
+                    if (address.indexOf('/login/') >= 0) {
+                        var userIdElement = $('#login_field');
+                        if (userIdElement.length) {
+                            userIdElement.val(profile.userId);
                             var userPassElement = $('#password');
                             if (userPassElement.length) {
-                                // TODO: Generate password
-                                userPassElement.val(newPass);
-                                var userPassElement2 = $('#password_confirmation');
-                                if (userPassElement2.length) {
-                                    // TODO: Generate password
-                                    userPassElement2.val(newPass);
-                                }
-                                //var form = userPassElement.parents('form');
-                                //var btn = form.find('[type="submit"]');
-                                //btn.click();
-                                //sendResponse(true);
+                                userPassElement.val(progress.data);
+                                var form = userPassElement.parents('form');
+                                var btn = form.find('[type="submit"]');
+                                btn.click();
+
+                                setTimeout(function () {
+                                    chrome.runtime.sendMessage({
+                                        'action': 'loginDone'
+                                    });
+                                }, 1000);
                             }
                         }
-                        break;
-                    default:
-                        break;
-                }
-                //alert('profile: ' + JSON.stringify(profile));
-                sendResponse(isReminder);
-                break;
-            default:
-                break;
-        }
+                    }
+                    break;
+            }
+            console.log('matched', JSON.stringify(arguments));
+        });
     });
 }
